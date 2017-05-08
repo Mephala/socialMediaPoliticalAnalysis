6d0acafdbe2575c57b023c73e8837eb724e520d5
Mephala <gokhan.ozgozen@gmail.com>
2017-05-08 17:27:55 +0300

    Implementing word analysis and linear regression model.

--- ptnla/src/test/java/TestPoliticianCreation.java
+++ ptnla/src/test/java/TestPoliticianCreation.java
@@ -1,14 +1,9 @@
-import com.gokhanozg.ptnla.FacebookTrendInterval;
-import com.gokhanozg.ptnla.HibernateUtil;
-import com.gokhanozg.ptnla.Politician;
 import mockit.integration.junit4.JMockit;
-import org.hibernate.Session;
+import org.ejml.simple.SimpleMatrix;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.math.BigDecimal;
-import java.util.Date;
-import java.util.UUID;
+import static junit.framework.TestCase.fail;
 
 /**
  * Created by mephala on 4/24/17.
@@ -21,29 +16,57 @@ public class TestPoliticianCreation {
     public void testCreatingAPoliticianWithTrend() {
 
         try {
-            Politician p = new Politician();
-            p.setPoliticianId(UUID.randomUUID().toString());
-            p.setPoliticianTurkishName("Gökhan Özgözen");
-            p.setPoliticianTwitterAccountName("gokhanOzgozen");
-            Session session = HibernateUtil.getSessionFactory().openSession();
-            session.beginTransaction();
-            session.saveOrUpdate(p);
-            FacebookTrendInterval trendInterval = new FacebookTrendInterval();
-            trendInterval.setStart(new Date());
-            Date d = new Date();
-            d.setTime(d.getTime() + (1000 * 60 * 60 * 24 * 7));
-            trendInterval.setEnd(d);
-//            trendInterval.setPolitician(p);
-            trendInterval.setPopulationChange(new BigDecimal(-40.36));
-            trendInterval.setTrendId(UUID.randomUUID().toString());
-            session.saveOrUpdate(trendInterval);
-            session.getTransaction().commit();
-            session.close();
+//            Politician p = new Politician();
+//            p.setPoliticianId(UUID.randomUUID().toString());
+//            p.setPoliticianTurkishName("Gökhan Özgözen");
+//            p.setPoliticianTwitterAccountName("gokhanOzgozen");
+//            Session session = HibernateUtil.getSessionFactory().openSession();
+//            session.beginTransaction();
+//            session.saveOrUpdate(p);
+//            FacebookTrendInterval trendInterval = new FacebookTrendInterval();
+//            trendInterval.setStart(new Date());
+//            Date d = new Date();
+//            d.setTime(d.getTime() + (1000 * 60 * 60 * 24 * 7));
+//            trendInterval.setEnd(d);
+////            trendInterval.setPolitician(p);
+//            trendInterval.setPopulationChange(new BigDecimal(-40.36));
+//            trendInterval.setTrendId(UUID.randomUUID().toString());
+//            session.saveOrUpdate(trendInterval);
+//            session.getTransaction().commit();
+//            session.close();
         } catch (Throwable t) {
             t.printStackTrace();
         }
 
     }
+
+    @Test
+    public void matrixStuff() {
+        try {
+            SimpleMatrix y = new SimpleMatrix(4, 1);
+            y.setRow(0, 0, 4d);
+            y.setRow(1, 0, 8d);
+            y.setRow(2, 0, 16d);
+            y.setRow(3, 0, 32d);
+            SimpleMatrix A = new SimpleMatrix(4, 1);
+            A.setRow(0, 0, 2d);
+            A.setRow(1, 0, 4d);
+            A.setRow(2, 0, 8d);
+            A.setRow(3, 0, 16d);
+            System.out.println(y);
+            System.out.println(A);
+            System.out.println(A.transpose());
+            System.out.println(A.transpose().mult(A));
+            System.out.println(A.transpose().mult(A).invert());
+            System.out.println(A.transpose().mult(A).invert().mult(A.transpose().mult(y)));
+
+
+        } catch (Throwable t) {
+            t.printStackTrace();
+            fail();
+        }
+    }
+
 
 
 }
