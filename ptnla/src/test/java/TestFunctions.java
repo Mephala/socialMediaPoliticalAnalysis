94cf4e34f2226956df9ccdc49c8fabfc5cd6df39
mephala <gokhan.ozgozen@gmail.com>
2017-05-10 01:23:38 +0300

    Implementing prediction model and CLI.

--- ptnla/src/test/java/TestFunctions.java
+++ ptnla/src/test/java/TestFunctions.java
@@ -2,6 +2,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gokhanozg.ptnla.SerializableMatrix;
 import mockit.integration.junit4.JMockit;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
 import org.ejml.simple.SimpleMatrix;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -99,6 +100,88 @@ public class TestFunctions {
         File file = new File(url.getPath());
         String value = FileUtils.readFileToString(file);
         return objectMapper.readValue(value, tClass);
+    }
+
+    @Test
+    public void testCoefficientPrecision() {
+        try {
+            SimpleMatrix y = new SimpleMatrix(2, 1);
+            SimpleMatrix A = new SimpleMatrix(2, 3);
+            y.setRow(0, 0, 106);
+            y.setRow(1, 0, 120);
+            A.setRow(0, 0, 3, 5, 10);
+            A.setRow(1, 0, 4, 6, 11);
+            System.out.println(y);
+            System.out.println(A);
+            System.out.println(A.transpose().mult(A).invert().mult(A.transpose().mult(y)));
+
+        } catch (Throwable t) {
+            t.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    public void testApacheOLSRegression() {
+        try {
+            OLSMultipleLinearRegression regression = new OLSMultipleLinearRegression();
+            double[] y = new double[]{11.0, 12.0, 13.0, 14.0, 15.0, 16.0};
+            double[][] x = new double[6][];
+            x[0] = new double[]{0, 3, 8, 2, 0};
+            x[1] = new double[]{2.0, 0, 0, 0, 0};
+            x[2] = new double[]{0, 3.0, 0, 0, 0};
+            x[3] = new double[]{0, 0, 4.0, 0, 0};
+            x[4] = new double[]{0, 3, 0, 5.0, 0};
+            x[5] = new double[]{0, 0, 0, 0, 6.0};
+            regression.newSampleData(y, x);
+            double[] beta = regression.estimateRegressionParameters();
+
+            double[] residuals = regression.estimateResiduals();
+
+            double[][] parametersVariance = regression.estimateRegressionParametersVariance();
+
+            double regressandVariance = regression.estimateRegressandVariance();
+
+            double rSquared = regression.calculateRSquared();
+
+            double sigma = regression.estimateRegressionStandardError();
+            System.out.println(sigma);
+
+
+        } catch (Throwable t) {
+            t.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    public void testOLSSimpleProblem() {
+        try {
+            OLSMultipleLinearRegression regression = new OLSMultipleLinearRegression();
+            double[] y = new double[]{106, 120, 190, 167};
+            double[][] x = new double[4][];
+            x[0] = new double[]{3, 5, 10, 5, 7};
+            x[1] = new double[]{4, 6, 11, 5, 6};
+            x[2] = new double[]{2, 7, 13, 8, 11};
+            x[3] = new double[]{4, 8, 11, 4, 5};
+            regression.newSampleData(y, x);
+            double[] beta = regression.estimateRegressionParameters();
+
+            double[] residuals = regression.estimateResiduals();
+
+            double[][] parametersVariance = regression.estimateRegressionParametersVariance();
+
+            double regressandVariance = regression.estimateRegressandVariance();
+
+            double rSquared = regression.calculateRSquared();
+
+            double sigma = regression.estimateRegressionStandardError();
+            System.out.println(sigma);
+
+        } catch (Throwable t) {
+            t.printStackTrace();
+            fail();
+        }
     }
 
 
